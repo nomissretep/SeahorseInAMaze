2110d8052ca22c26683288edf1de28e39990512c
Falk Selker <fselker@imedv03.ime.kfa-juelich.de>
2013-06-03 12:51:43 +0200

    Erste Version von Client und ServerContext

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,13 +1,110 @@
 package client;
 
+import generated.MazeCom;
+import generated.WinMessageType;
+
+import java.io.IOException;
+
+import javax.xml.bind.JAXBException;
+
+import spieler.SimpleKI;
+import spieler.Spieler;
+
+
 public class Client {
-	ServerContext context;
-	public Client(String hostname, int port){
-		
+	private ServerContext context;
+	private String hostname;
+	private int port;
+	private int id;
+	public boolean cont;
+	private Spieler spieler;
+
+	public Client(String hostname, int port) {
+		this.hostname = hostname;
+		this.port = port;
+		context = new ServerContext();
+
+		spieler = new SimpleKI();//andere KIs oder menschliche Nutzer hier einstellbar
 	}
-	
-	public void run(){
-		
+
+	public void run() {
+		try {
+			id = context.login(hostname, port, spieler.getName());
+		} catch (IOException e) {
+			System.out.println("Login fehlgeschlagen");
+			e.printStackTrace();
+		} catch (JAXBException e) {
+			System.out.println("Fehler bei der Uebertragung");
+			e.printStackTrace();
+
+		}
+
+		boolean sent = false;// gibt an, ob die letzte Nachricht ein awaitMove(true) oder ein Accept(false) war
+
+		while (cont) {// Der Ablauf des Programms
+			MazeCom mc = context.receive();
+			MazeCom answer = new MazeCom();
+
+			switch (mc.getMcType()) {
+
+			case AWAITMOVE:
+				answer.setMoveMessage(spieler.doTurn(mc.getAwaitMoveMessage().getBoard()));
+				try{
+					context.send(answer, id);
+				}catch(IOException e){
+					System.out.println("Verbindungsfehler beim Senden: ");
+					e.printStackTrace();
+					cont =false;
+				}catch(JAXBException e){
+					System.out.println("Fehler beim Marshallen");
+					e.printStackTrace();
+					cont = false;
+				}
+				sent = true;
+				break;
+			case WIN:
+				WinMessageType winner = mc.getWinMessage();
+				System.out.println(winner.toString() + "hat das Spiel Gewonnen");
+				cont = false;
+				break;
+			case ACCEPT://TODO
+				switch (mc.getAcceptMessage().getErrorCode()) {
+				case NOERROR:
+					if (sent) {
+						sent = false;
+						break;
+					}
+				case AWAIT_LOGIN: //u.U. login wiederholen
+					;
+				case AWAIT_MOVE:
+					;
+				case ERROR:
+					;
+				case ILLEGAL_MOVE: // hier alternativen zug auswaehlen
+					;
+				case TIMEOUT:
+					;
+				case TOO_MANY_TRIES:
+					;
+				default:
+					;
+
+				}
+			case DISCONNECT:
+				;
+			case LOGIN:
+				;
+			case LOGINREPLY:
+				;
+			case MOVE:
+				;
+			default:
+				System.out.println("Ungueltiger NachrichtenTyp:");
+				System.out.println(mc.getMcType());
+				cont = false;
+			}
+		}
+
 	}
 
 }
--- src/client/ServerContext.java
+++ src/client/ServerContext.java
@@ -1,8 +1,76 @@
 package client;
 
+import generated.LoginMessageType;
+import generated.LoginReplyMessageType;
+import generated.MazeCom;
+import generated.ObjectFactory;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.net.Socket;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import networking.UTFInputStream;
+import networking.UTFOutputStream;
+
 public class ServerContext {
-	Socket socket;
+	private Socket socket;
+	private UTFInputStream utfin;
+	private UTFOutputStream utfout;
+
+	private JAXBContext jc;
+	private ObjectFactory of;
+	private javax.xml.bind.Marshaller m;
+	private Unmarshaller um;
+
+	public ServerContext() {
+		try {
+			of = new ObjectFactory();
+			jc = JAXBContext.newInstance(MazeCom.class);
+			m = jc.createMarshaller();
+		} catch (JAXBException e) {
+			System.out.println("Fehler beim Erstellen der JAXB-Dateien:");
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+
+	public int login(String hostname, int port, String name) throws IOException, JAXBException{
+			socket = new Socket(hostname, port);
+			utfout=new UTFOutputStream(socket.getOutputStream());
+			utfin=new UTFInputStream(socket.getInputStream());
+			MazeCom mc = new MazeCom();
+			
+			// Erstellen der Login- Nachricht und abschicken 
+			LoginMessageType lmt= of.createLoginMessageType();
+			lmt.setName(name); //TODO
+			StringWriter sw = new StringWriter();
+			mc.setLoginMessage(lmt);
+			m.marshal(mc, sw);
+			utfout.writeUTF8(sw.toString());
+			
+			StringReader sr = new StringReader(utfin.readUTF8());
+			//empfangen der login-reply-message
+			mc = (MazeCom) um.unmarshal(sr);
+			LoginReplyMessageType lrt = mc.getLoginReplyMessage();
+			int id = lrt.getNewID();//hier noch diverse Fehlermeldungen in der LoginreplyMessage beachten
+			
+			return id;//soll vom Server eine id bekommen, die dann benutzt wird
+	}
+
+	public void send(MazeCom mc, int id) throws IOException, JAXBException{
+		StringWriter sw = new StringWriter();
+		m.marshal(mc, sw);
+		utfout.writeUTF8(sw.toString());
+		
+	}
+
+	public MazeCom receive() {
+		return new MazeCom();
+	}
 
 }
