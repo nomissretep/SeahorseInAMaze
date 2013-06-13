7274292a3609626232899b144ee92de00905b892
MaPePeR <mapeper@example.com>
2013-06-13 17:08:39 +0200

    Improve Client to take a KI-Name as an Commandline-Argument

--- src/client/Main.java
+++ src/client/Main.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.net.UnknownHostException;
 
 import spieler.DecisionKI;
+import spieler.ISpieler;
+import spieler.RekursiveDecisionKI;
+import spieler.Spieler;
 
 public class Main {
 	public static final String defaultHostname = "localhost";
@@ -13,38 +16,78 @@ public class Main {
 			IOException {
 		String hostname;
 		int port;
-		if (args.length == 2) {
-			hostname = args[0];
-			try {
-				port = Integer.parseInt(args[1]);
-			} catch (NumberFormatException e) {
-				showUsage();
-				return;
-			}
-		} else if (args.length == 1) {
-			if (args[0].startsWith("-")) {
-				showUsage();
-				return;
-			}
-			hostname = args[0];
-			port = defaultPort;
-			System.out.format("Using default-Port: %d\n", port);
-		} else if (args.length == 0) {
-			hostname = defaultHostname;
-			port = defaultPort;
-			showUsage();
-			System.out.format("Using defaults: %s %d\n", hostname, port);
+		ISpieler ki;
+		
+		if(args.length >= 1) {
+			ki = tryToLoadSpieler(args[0]);
+			if(ki == null) return;
 		} else {
 			showUsage();
 			return;
 		}
+		
+		if (args.length >= 2) {
+			hostname = args[1];
+		} else {
+			System.out.println("Using Default-Hostname: localhost");
+			hostname = "localhost";
+		}
+		if(args.length == 3) {
+			try {
+				port = Integer.parseInt(args[2]);
+			} catch (NumberFormatException e) {
+				showUsage();
+				return;
+			}
+		} else if(args.length > 3) {
+			System.out.println("Too many arguments!");
+			showUsage();
+			return;
+		} else {
+			System.out.println("Using Default-Port: 5123");
+			port = 5123;
+		}
+			
 		Client client = new Client(hostname, port);
-		client.run(new DecisionKI());
+		client.run(ki);
 	}
 
 	public static void showUsage() {
-		System.out.format("USAGE: [HOSTNAME=%s] [PORT=%d]\n", defaultHostname,
+		System.out.format("USAGE: KI-Name [HOSTNAME=%s] [PORT=%d]\n", defaultHostname,
 				defaultPort);
+	}
+	public static ISpieler tryToLoadSpieler(String spielername) {
+		ClassLoader cl = Main.class.getClassLoader();
+		Class<?> spielerInterface;
+		try {
+			spielerInterface = cl.loadClass("spieler.ISpieler");
+		} catch (ClassNotFoundException e1) {
+			throw new RuntimeException(e1);
+		}
+		
+		try {
+			Class<?> clazz = cl.loadClass("spieler."+spielername);
+			if(clazz != null) {
+				if(spielerInterface.isAssignableFrom(clazz)) {
+					try {
+						return (ISpieler) clazz.newInstance();
+					} catch (InstantiationException e) {
+						System.out.println("Coudn't instantiate spieler."+spielername);
+						return null;
+					} catch (IllegalAccessException e) {
+						System.out.println("Coudn't access spieler."+spielername);
+						return null;
+					}
+				} else {
+					System.out.println("KI '"+spielername+"' does not implement ISpieler");
+					return null;
+				}
+			}
+		} catch (ClassNotFoundException e) {
+			System.out.println("Coudn't find Class: spieler."+spielername);
+			return null;
+		}
+		return null;
 	}
 
 }
