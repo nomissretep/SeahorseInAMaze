7c8477503ebc6a0a3319d467a6cafd233d50fc9e
nomissretep <nomissretep@yahoo.de>
2013-06-18 09:23:22 +0200

    Strategie erweitert mit variabler Bewertung

--- src/assessment/Strategie.java
+++ src/assessment/Strategie.java
@@ -40,4 +40,58 @@ public class Strategie implements IStrategie {
 		h.value=h.value*10+(24-gegnerSchaetze);
 		return h;
 	}
+	
+	
+	public High bewerte(Board board,int nah, int wegnah,int schaetze,int bewegung,int[] gegner) {
+		
+		Assessment as=new Assessment(board);
+		//Boolean Array, True = Position hat schatz.
+		boolean t[][]=as.findTreasures();		
+		//Wo kann ich hingehen
+		boolean[][] iGo=as.whereICanGo();
+		//Von wo aus komm ich zum schatz
+		boolean[][] tWay=as.whereToGo(board.getTreasurePosition());
+		//Abstand zu Treasure von jeder Position
+		int [][]tWeights=as.weights(board.getTreasurePosition());//28-100		
+		//Entfernung zu wegen die zum schatz fuehren
+		int [][]tWayWeights=as.nearTheWay(tWay, 1); //1 = umgebung die gewertet werden soll
+		//10*(a+b)
+		
+		
+		
+		Assessmentfield.increase(tWeights, nah);
+		Assessmentfield.increase(tWayWeights, wegnah);
+		
+		//Wieviel Felder kann ich erreichen
+		int fields=Assessmentfield.count(iGo);		
+		//Wieviele Schaetze kann ich erreichen
+		int schaetze2=Assessmentfield.count(Assessmentfield.and(iGo,t));
+		
+		
+		
+		int[][]tadd=Assessmentfield.add(tWayWeights, tWeights);
+		int[][]tposible=Assessmentfield.mult(iGo,tadd);
+		High h=Assessmentfield.findHigh(tposible);
+		
+		
+
+		
+		h.value+=fields*bewegung+schaetze2*schaetze;
+		
+		
+		
+		
+		//bitte ueberabeiten
+		for(int i=0;i<gegner.length;i++)
+		{
+			Position pGegner=board.getSpielerPositions().values().iterator().next();//diesen Spieler tauschen		
+			boolean[][] gway=as.whereToGo(pGegner);
+			int gegnerSchaetze=Assessmentfield.count(Assessmentfield.and(gway,t));	
+		
+			h.value+=gegner[i]*(24-gegnerSchaetze);
+		}
+		return h;
+	}
+	
+	
 }
--- src/spieler/strategies/ICanWinStrategie.java
+++ src/spieler/strategies/ICanWinStrategie.java
@@ -1,14 +1,15 @@
 package spieler.strategies;
 
 import ourGenerated.Board;
+import assessment.Assessment;
+import assessment.Assessmentfield;
 import assessment.High;
 import assessment.IStrategie;
 
 public class ICanWinStrategie implements IStrategie {
 
 	@Override
-	public High bewerte(Board b) {
-		// TODO Auto-generated method stub
+	public High bewerte(Board board) {
 		return null;
 	}
 
